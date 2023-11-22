fa91eb3ec5745567f1e98afe82f8bb76fcec9585
Lexi <mlexifarris@gmail.com>
2023-11-21 18:29:42 -0600

    added destroy action

--- config/routes.rb
+++ config/routes.rb
@@ -3,16 +3,17 @@ Rails.application.routes.draw do
   get "/users" => "users#index"
   post "/users" => "users#create"
   get "/users/:id" => "users#show"
+  delete "/users/:id" => "users#destroy"
 
   #To Dos
   get "/to_dos" => "to_dos#index"
+  
   #Categories
-
   get "/categories" => "categories#index"
   post "/categories" => "categories#create"
   get "categories/:id" => "categories#show"
   patch "categories/:id" => "categories#update"
-  delete "/users/:id" => "users#destroy"
+  delete "categories/:id" => "categories#destroy"
 
   get "/category_to_dos" => "category_to_do#index"
 end
